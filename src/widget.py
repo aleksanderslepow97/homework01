7e6f2c990585158ca80a84b4cec5660764a9b244
Aleksander <sanya.slepow2015@yandex.ru>
2024-07-04 15:23:42 +0500

    commit4

--- src/widget.py
+++ src/widget.py
@@ -1,15 +1,14 @@
 from masks import get_mask_card_number
-from masks import get_mask_account
 
 
 def mask_account_card(string_number: str) -> str:
     """функция обработки информации о картах и счетах"""
     if len(string_number.split()[-1]) == 16:
-        new_number = get_mask_card_number(string_number.split()[-1])
-        result = f"{string_number[:-16]}{new_number}"
+        new_numb = get_mask_card_number(string_number.split()[-1])
+        result = f"{string_number[:-16]}{new_numb}"
     elif len(string_number.split()[-1]) == 20:
-        new_number =  get_mask_card_number(string_number.split()[-1])
-        result = f"{string_number[:-20]}{new_number}"
+        new_numb = get_mask_card_number(string_number.split()[-1])
+        result = f"{string_number[:-20]}{new_numb}"
     return result
 
 
