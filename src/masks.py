c2e42d41eb696753da3a56f4ab56cfa05d532592
Aleksander <sanya.slepow2015@yandex.ru>
2024-07-17 13:58:16 +0500

    develop25

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,27 @@
-def get_mask_card_number(card_number: str) -> str | None:
-    """функция маскировки номера карты"""
-    if card_number.isdigit() and len(card_number) == 16:
-        return f"{card_number[:4]} {card_number[4:6]}{"*" * 2} {"*" * 4} {card_number[12:]}"
+import logging
+
+logger = logging.getLogger("masks")
+file_handler = logging.FileHandler("loggers_info.txt")
+file_formatter = logging.Formatter("%(asctime)s %(filename)s %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
-def get_mask_account(acc_number: str) -> str | None:
-    """функция маскировки номера счета"""
-    if acc_number.isdigit() and len(acc_number) == 20:
-        return f"{'*' * 2}{acc_number[-4::]}"
+def masked_card_num(num: str) -> str:
+    """функция принимает номер карты и возвращает замаскированный номер
+    @rtype: object
+    """
+    logger.info(f"start masked_card_num {num}")
+    number = 4
+    result = num[:number] + " " + num[number : number + 2] + "** ****" + " " + num[number + 8 :]
+    logger.info(f"mask {result}")
+    return result
+
+
+def masked_account_num(num: str) -> str:
+    """функция принимает номер счёта и возвращает замаскированный номер"""
+    logger.info(f"start masked_account_num {num}")
+    result = "**" + num[-4:]
+    logger.info(f"mask {result}")
+    return result
