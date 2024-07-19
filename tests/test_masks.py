77f8283797476e9f597b909c9e44397f9a58bc6d
Aleksander <sanya.slepow2015@yandex.ru>
2024-07-19 16:51:19 +0500

    develop28

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,24 +1,32 @@
 import pytest
 
-from src.masks import masked_account_num, masked_card_num
+from src.masks import masked_card_num, masked_account_num
+
+
+@pytest.mark.parametrize("string, expected_result", [
+    ("MasterCard 7158300734726758", "MasterCard 7158 30** **** 6758"),
+    ("Счет 12345678901234567890", "Счет **7890"),
+])
+def test_masked_card_num(string, expected_result):
+    assert masked_card_num(string) == expected_result
 
 
 @pytest.fixture
-def coll():
+def date():
     return "2018-07-11T02:26:18.671407"
 
 
-@pytest.mark.parametrize(
-    "x, expected",
-    [
-        ("Visa Platinum 7000 7922 8960 6361", "Visa Platinum 7000 79** **** 6361"),
-        ("Maestro 7000 7922 8960 6361", "Maestro 7000 79** **** 6361"),
-        ("Счет 73654108430135874305", "Счет **4305"),
-    ],
-)
-def test_add(x, expected):
-    assert masked_card_num(x) == expected
+@pytest.mark.parametrize("string, expected_result", [
+    ("7158300734726758", "7158 30** **** 6758"),
+    ("7158300734726759", "7158 30** **** 6759"),
+])
+def test_masked_card_num(string, expected_result):
+    assert masked_card_num(string) == expected_result
 
 
-def test_dated(coll):
-    assert masked_account_num(coll) == "11.07.2018"
+@pytest.mark.parametrize("string, expected_result", [
+    ("12345678901234567340", "**7340"),
+    ("12345678901234567890", "**7890"),
+])
+def test_masked_account_num(string, expected_result):
+    assert masked_account_num(string) == expected_result
